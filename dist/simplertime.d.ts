859c92ae88b00b6a46efe8dd12e7c84c30592659
David Piepgrass <qwertie256@gmail.com>
2018-06-30 12:48:26 -0600

    Rebuild (1.6K minified).

--- dist/simplertime.d.ts
+++ dist/simplertime.d.ts
@@ -1,33 +1,73 @@
 /**
- * Parses a string into a Date. Supports several formats: "12", "1234",
- * "12:34", "12:34pm", "12:34 PM", "12:34:56 pm", and "12:34:56.789".
+ * Parses a string into a number of seconds since midnight. Supports several
+ * formats: "12", "1234", "12:34", "12:34pm", "12:34 PM", "12:34:56 pm", and "12:34:56.789".
  * The time must be at the beginning of the string but can have leading spaces.
  * Anything is allowed after the time as long as the time itself appears to
  * be valid, e.g. "12:34*Z" is OK but "12345" is not.
  * @param {string} t Time string, e.g. "1435" or "2:35 PM" or "14:35:00.0"
- * @param {Date|undefined} localDate If this parameter is provided, setHours
- *        is called on it. Otherwise, setUTCHours is called on 1970/1/1.
- * @returns {Date|undefined} The parsed date, if parsing succeeded.
+ * @param {Date|undefined} localDate If this parameter is provided, it is
+ *        cloned and setHours is called on the clone, which is returned.
+ * @returns {Date|number|undefined} The parsed date/time, if parsing succeeded.
+ *        If a localDate was provided, this is a Date, otherwise it is a number
+ *        of milliseconds since 00:00 midnight. In the latter case, the number
+ *        is not wrapped in a Date because it could be interpreted as a time
+ *        on January 1, 1970, which does not necessarily round-trip correctly
+ *        with timeToString because Daylight Savings may be different in
+ *        January 1970 than it is today. Returning a number makes it clear that
+ *        the time is not intended to be interpreted as being in 1970.
  */
-export declare function parseTime(t: string, localDate?: Date): Date | undefined;
+export declare function parseTime(t: string, localDate?: Date): Date | number | undefined;
 /** Calls valueOf() on the value if it is not already a number */
 export declare function unwrapDate(date: {
     valueOf(): number;
 } | number): number;
-/** String used by `timeToString` to represent AM/PM (default: " am" and " pm") */
-export declare var amString: string, pmString: string;
+/** Options accepted as second parameter of `timeToString` */
+export interface TimeFormatOptions {
+    /** String added by `timeToString` to represent AM (default: " AM") */
+    am?: string;
+    /** String added by `timeToString` to represent PM (default: " PM") */
+    pm?: string;
+    /** Optional string added instead of AM or PM for evenings (6pm to 1am) */
+    evening?: string;
+    /** If true, a 24-hour clock is used and AM/PM is hidden */
+    use24hourTime?: boolean;
+    /** Whether to include seconds in the output (null causes seconds
+     *  to be shown only if seconds or milliseconds are nonzero) */
+    showSeconds?: boolean | null;
+    /** Whether to include milliseconds in the output when seconds are
+     *  shown (null shows ms only if nonzero; this field is treated as
+     *  false if showSeconds is false.) */
+    showMilliseconds?: boolean | null;
+    /** Whether to print the time as UTC or local time
+     *  (ignored by timeToString) */
+    utc?: boolean;
+}
+/** Default options */
+export declare var defaultTimeFormat: {
+    am: string;
+    pm: string;
+    evening: undefined;
+    use24hourTime: boolean;
+    showSeconds: null;
+    showMilliseconds: null;
+    utc: undefined;
+};
 /**
- * Converts a Date (or number of millisec since unix epoch) to a string showing the time of day in the GMT time zone.
+ * Converts a Date (or number of millisec since unix epoch) to a string
+ * showing the time of day in the UTC/GMT time zone.
  * @param time The Date/time to make a string from
- * @param use24hourTime If false, am or pm time is used.
- * @param showSeconds Whether to show the number of seconds (and milliseconds, if nonzero)
+ * @param opt Formatting options (optional). The function won't modify it.
  */
-export declare function timeToStringUTC(time: Date | number, use24hourTime: boolean, showSeconds?: boolean): string;
+export declare function timeToStringUTC(time: Date | number, opt?: TimeFormatOptions): string;
 /**
- * Converts a Date (or number of millisec since unix epoch) to a string showing the time of day.
- * @param time The Date/time to make a string from
- * @param use24hourTime If false, am or pm time is used.
- * @param showSeconds Whether to show the number of seconds (and milliseconds, if nonzero)
- * @param utc Whether the time should be interpreted as a local time or as UTC
+ * Converts a Date (or number of millisec since unix epoch) to a string
+ * showing the time of day.
+ * @param time The Date/time to make a string from. It is expected to be
+ *   a UTC time that you want adjusted to local time (unless you use
+ *   `utc:false` to prevent adjustment). Note: time zones change over
+ *   time, e.g. for daylight savings. Therefore, if you want to display
+ *   a local time from the past, you must store it in a Date object with
+ *   the correct date in order to get a correct time zone adjustment.
+ * @param opt String formatting options (optional). The function won't modify it.
  */
-export declare function timeToString(time: Date | number, use24hourTime: boolean, showSeconds?: boolean, utc?: boolean): string;
+export declare function timeToString(time: Date | number, opt?: TimeFormatOptions): string;
--- dist/simplertime.js
+++ dist/simplertime.js
@@ -10,15 +10,22 @@
     "use strict";
     Object.defineProperty(exports, "__esModule", { value: true });
     /**
-     * Parses a string into a Date. Supports several formats: "12", "1234",
-     * "12:34", "12:34pm", "12:34 PM", "12:34:56 pm", and "12:34:56.789".
+     * Parses a string into a number of seconds since midnight. Supports several
+     * formats: "12", "1234", "12:34", "12:34pm", "12:34 PM", "12:34:56 pm", and "12:34:56.789".
      * The time must be at the beginning of the string but can have leading spaces.
      * Anything is allowed after the time as long as the time itself appears to
      * be valid, e.g. "12:34*Z" is OK but "12345" is not.
      * @param {string} t Time string, e.g. "1435" or "2:35 PM" or "14:35:00.0"
-     * @param {Date|undefined} localDate If this parameter is provided, setHours
-     *        is called on it. Otherwise, setUTCHours is called on 1970/1/1.
-     * @returns {Date|undefined} The parsed date, if parsing succeeded.
+     * @param {Date|undefined} localDate If this parameter is provided, it is
+     *        cloned and setHours is called on the clone, which is returned.
+     * @returns {Date|number|undefined} The parsed date/time, if parsing succeeded.
+     *        If a localDate was provided, this is a Date, otherwise it is a number
+     *        of milliseconds since 00:00 midnight. In the latter case, the number
+     *        is not wrapped in a Date because it could be interpreted as a time
+     *        on January 1, 1970, which does not necessarily round-trip correctly
+     *        with timeToString because Daylight Savings may be different in
+     *        January 1970 than it is today. Returning a number makes it clear that
+     *        the time is not intended to be interpreted as being in 1970.
      */
     function parseTime(t, localDate) {
         // ?: means non-capturing group and ?! is zero-width negative lookahead
@@ -36,10 +43,12 @@
                 hour += 12;
             if (hour === 24)
                 hour = 0;
-            var date = new Date(localDate !== undefined ? localDate.valueOf() : 0);
-            var set = (localDate !== undefined ? date.setHours : date.setUTCHours);
-            set.call(date, hour, min, sec, ms);
-            return date;
+            if (localDate !== undefined) {
+                var date = new Date(localDate.valueOf());
+                date.setHours(hour, min, sec, ms);
+                return date;
+            }
+            return ((hour * 60 + min) * 60 + sec) * 1000 + ms;
         }
         return undefined;
     }
@@ -49,16 +58,27 @@
         return typeof date === 'number' ? date : date.valueOf();
     }
     exports.unwrapDate = unwrapDate;
-    /** String used by `timeToString` to represent AM/PM (default: " am" and " pm") */
-    exports.amString = " am", exports.pmString = " pm";
+    /** Default options */
+    exports.defaultTimeFormat = {
+        am: " AM", pm: " PM", evening: undefined, use24hourTime: false,
+        showSeconds: null, showMilliseconds: null, utc: undefined
+    };
+    // Helper functions for timeToStringUTC
+    function twoDigit(n) { var s = n.toString(); return s.length >= 2 ? s : '0' + s; }
+    function threeDigit(n) { var s = twoDigit(n); return s.length >= 3 ? s : '0' + s; }
+    function get(opt, name) {
+        return opt === undefined || opt[name] === undefined ? exports.defaultTimeFormat[name] : opt[name];
+    }
     /**
-     * Converts a Date (or number of millisec since unix epoch) to a string showing the time of day in the GMT time zone.
+     * Converts a Date (or number of millisec since unix epoch) to a string
+     * showing the time of day in the UTC/GMT time zone.
      * @param time The Date/time to make a string from
-     * @param use24hourTime If false, am or pm time is used.
-     * @param showSeconds Whether to show the number of seconds (and milliseconds, if nonzero)
+     * @param opt Formatting options (optional). The function won't modify it.
      */
-    function timeToStringUTC(time, use24hourTime, showSeconds) {
+    function timeToStringUTC(time, opt) {
         time = unwrapDate(time);
+        var op2 = exports.defaultTimeFormat;
+        opt = opt || op2;
         var perday = 24 * 60 * 60000, ms = ((time % perday) + perday) % perday;
         var sec = ms / 1000 | 0;
         ms %= 1000;
@@ -66,34 +86,37 @@
         sec %= 60;
         var hour = min / 60 | 0;
         min %= 60;
-        var secString = (showSeconds === undefined ? sec != 0 || ms != 0 : showSeconds) ? ':' + twoDigit(sec) : '';
-        var suffix = secString !== '' && ms != 0 ? secString + '.' + threeDigit(ms) : secString;
-        var result;
-        if (use24hourTime)
-            result = twoDigit(hour) + ':' + twoDigit(min) + suffix;
+        var ssec = get(opt, 'showSeconds'), sms = get(opt, 'showMilliseconds');
+        var secString = (ssec == null ? sec + ms : ssec) ? ':' + twoDigit(sec) : '';
+        var suffix = (sms == null ? secString !== '' && ms : sms) ? secString + '.' + threeDigit(ms) : secString;
+        if (get(opt, 'use24hourTime'))
+            return twoDigit(hour) + ':' + twoDigit(min) + suffix;
         else {
-            var pm = hour >= 12;
-            result = ((hour + 11) % 12 + 1).toString() + ':' + twoDigit(min) + suffix + (pm ? exports.pmString : exports.amString);
+            var eve = get(opt, 'evening');
+            var pm = eve && (hour < 2 || hour > 17) ? eve : get(opt, hour >= 12 ? 'pm' : 'am');
+            return ((hour + 11) % 12 + 1).toString() + ':' + twoDigit(min) + suffix + pm;
         }
-        return result;
-        function twoDigit(n) { var s = n.toString(); return s.length >= 2 ? s : '0' + s; }
-        function threeDigit(n) { var s = twoDigit(n); return s.length >= 3 ? s : '0' + s; }
     }
     exports.timeToStringUTC = timeToStringUTC;
     /**
-     * Converts a Date (or number of millisec since unix epoch) to a string showing the time of day.
-     * @param time The Date/time to make a string from
-     * @param use24hourTime If false, am or pm time is used.
-     * @param showSeconds Whether to show the number of seconds (and milliseconds, if nonzero)
-     * @param utc Whether the time should be interpreted as a local time or as UTC
+     * Converts a Date (or number of millisec since unix epoch) to a string
+     * showing the time of day.
+     * @param time The Date/time to make a string from. It is expected to be
+     *   a UTC time that you want adjusted to local time (unless you use
+     *   `utc:false` to prevent adjustment). Note: time zones change over
+     *   time, e.g. for daylight savings. Therefore, if you want to display
+     *   a local time from the past, you must store it in a Date object with
+     *   the correct date in order to get a correct time zone adjustment.
+     * @param opt String formatting options (optional). The function won't modify it.
      */
-    function timeToString(time, use24hourTime, showSeconds, utc) {
-        if (typeof time === 'number')
-            time = new Date(time);
-        var ms = time.valueOf();
-        if (utc !== true)
-            ms -= time.getTimezoneOffset() * 60000;
-        return timeToStringUTC(ms, use24hourTime, showSeconds);
+    function timeToString(time, opt) {
+        var ms = unwrapDate(time);
+        if (!opt || opt.utc !== true) {
+            // Bug fix: we can't use `new Date(time)` here because if the `time` has 
+            // no date, a time zone adjustment for January 1970 is used (may be wrong).
+            ms -= (typeof time === 'number' ? new Date() : time).getTimezoneOffset() * 60000;
+        }
+        return timeToStringUTC(ms, opt);
     }
     exports.timeToString = timeToString;
 });
